661784151e626af5943d9ceac71b04374cd14c40
GUllzarr <160250097+GUllzarr@users.noreply.github.com>
2024-06-02 00:42:07 -0700

    DOne real ATm applicatin

---
+++ script.js
@@ -0,0 +1,10 @@
+var amount = 50000
+var balance = +prompt("Please Enter Amount YOu want to withdrwa")
+  
+if(amount === balance)
+{
+    alert("YOu can able to withdraw")
+}
+else{
+    alert("Insuffient balance")
+}
